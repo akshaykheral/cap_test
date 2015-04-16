77326d1e892466e01bac6af77435fcb94776f257
akshaykheral <akshay.kheral@yahoo.com>
2015-04-16 11:42:20 +0530

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,15 @@
 RAILS_ROOT = File.dirname(__FILE__) + '/..'
 
-every 2.hours do  
-  rake "thinking_sphinx:index"  
+every :day, :at => '1:37pm', :roles => [:app] do
+  rake 'app:task' # will only be added to crontabs of :app servers
 end
 
-every :reboot do  
-  rake "thinking_sphinx:start"  
+every :hour, :roles => [:db] do
+  rake 'db:task' # will only be added to crontabs of :db servers
+end
+
+every :day, :at => '12:02am' do
+  command "run_this_everywhere" # will be deployed to :db and :app servers
 end
 
 every :friday, :at => "4am" do
